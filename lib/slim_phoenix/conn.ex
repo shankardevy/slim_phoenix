3b78c4ba37535b24455638fc1343bf5ee5165cd9
Shankar Dhanasekaran <shankar@opendrops.com>
2017-02-27 21:51:38 +0530

    Define helper functions to modify conn

--- lib/slim_phoenix/conn.ex
+++ lib/slim_phoenix/conn.ex
@@ -1,3 +1,11 @@
 defmodule SlimPhoenix.Conn do
   defstruct [:req_path, :res_code, :res_body]
+
+  def put_res_body(conn, body) do
+    Map.put(conn, :res_body, body)
+  end
+
+  def put_res_code(conn, code) do
+    Map.put(conn, :res_code, code)
+  end
 end
--- lib/slim_phoenix/router.ex
+++ lib/slim_phoenix/router.ex
@@ -1,21 +1,31 @@
 defmodule SlimPhoenix.Router do
+  import SlimPhoenix.Conn, only: [put_res_body: 2, put_res_code: 2]
+
   def call(conn) do
     content_for(conn.req_path, conn)
   end
 
   defp content_for("/", conn) do
-    %{conn | res_code: 200, res_body: "<h1>Home Page</h1>"}
+    conn
+    |> put_res_code(200)
+    |> put_res_body("<h1>Home Page</h1>")
   end
 
   defp content_for("/contact", conn) do
-    %{conn | res_code: 200, res_body: "<h1>Contact Page</h1>"}
+    conn
+    |> put_res_code(200)
+    |> put_res_body("<h1>Contact Page</h1>")
   end
 
   defp content_for("/about", conn) do
-    %{conn | res_code: 200, res_body: "<h1>About Page</h1>"}
+    conn
+    |> put_res_code(200)
+    |> put_res_body("<h1>About Page</h1>")
   end
 
   defp content_for(_, conn) do
-    %{conn | res_code: 404, res_body: "<h1>404 error</h1>"}
+    conn
+    |> put_res_code(404)
+    |> put_res_body("<h1>404 error</h1>")
   end
 end
